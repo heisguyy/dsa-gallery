7f01da04ebb5964c108cc51b46afc60aadd4d18b
Olajide Oluwatosin <oluwatosinaina424@gmail.com>
2023-07-23 20:59:01 +0100

    Add python implementation of selection sort
    This commit adds the python implementation of selection sort algorithm.
    It has a time complexity of O(n^2) and a space complexity of O(1).

--- implementation/Python/sorting.py
+++ implementation/Python/sorting.py
@@ -3,7 +3,7 @@ from typing import List
 
 def bubble_sort(arr: List[int]) -> List[int]:
     """
-    An implementation of the bubble sort algoirthm used to arrange 
+    An implementation of the bubble sort algoirthm(aka sinking sort) used to arrange 
     a mutable sequence of integers in a particular order.
 
     Args:
@@ -20,6 +20,26 @@ def bubble_sort(arr: List[int]) -> List[int]:
                 arr[i] = temp
     return arr
 
+def selection_sort(arr: List[int]) -> List[int]:
+    """
+    An implementation of the selection sort algorithm used to arrange mutable sequences into
+    in a particular order
+
+    Args:
+        arr (List[int]): A sequence with unsorted elements.
+
+    Returns:
+        List[int]: the sorted version of the sequence inputted.
+    """
+    for i, _ in enumerate(arr):
+        index_of_minimum_value = -1
+        for index, value in enumerate(arr[i:]):
+            if index_of_minimum_value == -1 or value < arr[i + index_of_minimum_value]:
+                index_of_minimum_value = index
+        arr[i], arr[i + index_of_minimum_value] = arr[i + index_of_minimum_value], arr[i]
+    return arr
+
 
 if __name__ == "__main__":
     print(bubble_sort([2,3,1,8,5,6,0]))
+    print(selection_sort([2,3,1,8,5,6,0]))
